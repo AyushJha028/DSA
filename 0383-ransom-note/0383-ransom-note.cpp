ed41da08f1e638b783b90803fdd93f1c4004cf93
Ayush Kumar Jha <ayushkumarj932@gmail.com>
2026-07-15 12:05:46 +0530

    Time: 0 ms (100%), Space: 11.6 MB (89.57%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.cpp
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -1,22 +1,13 @@
 class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
-        int n=ransomNote.size();
-        int m=magazine.size();
-        if(m<n)
-            return false;
-        if(n==0)
-            return true;
-        unordered_map<char,int> ransom;
-        unordered_map<char,int> mag;
+        int freq [26]={0};
+        for(auto &m:magazine){
+            freq[m-'a']++;
+        }
         for(auto &r:ransomNote){
-            ransom[r]++;
-        }
-        for(auto& m:magazine){
-            mag[m]++;
-        }
-        for(char ch='a';ch<='z';ch++){
-            if(ransom[ch] > mag[ch])
+            freq[r-'a']--;
+            if(freq[r -'a'] < 0)
                 return false;
         }
         return true;
