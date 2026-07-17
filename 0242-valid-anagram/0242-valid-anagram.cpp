3f478894a099a35e964e7a73e14eb43c126004e3
Ayush Kumar Jha <ayushkumarj932@gmail.com>
2026-07-17 11:45:26 +0530

    Time: 6 ms (22.67%), Space: 9.7 MB (76.44%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,20 +1,10 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        unordered_map<char,int> mpp1;
-        unordered_map<char,int> mpp2;
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
         if(s.size() > t.size())
             return false;
-        for(auto& ch:s){
-            mpp1[ch]++;
-        }
-        for(auto& ch:t){
-            mpp2[ch]++;
-        }
-        for(char ch='a';ch<='z';ch++){
-            if(mpp1[ch] != mpp2[ch])
-                return false;
-        }
-        return true;
+        return s==t;
     }
 };
