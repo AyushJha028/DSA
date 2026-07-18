83cfca214d8ae8020887790f69764b4c8c4fec89
Ayush Kumar Jha <ayushkumarj932@gmail.com>
2026-07-18 10:47:04 +0530

    Time: 2 ms (23.37%), Space: 8.6 MB (5.93%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -1,18 +1,16 @@
 class Solution {
 public:
     int newNum(int n){
-        int val=0;
-        while(n >0){
-            int dig= n%10;
-            val  += dig * dig;
+        int ans=0;
+        while(n>0){
+            int digit=n%10;
+            ans += digit * digit;
             n=n/10;
         }
-        return val;
+        return ans;
     }
-
     bool isHappy(int n) {
         unordered_map<int,int> mpp;
-
         while(mpp.find(n) == mpp.end()){
             mpp[n]++;
             n=newNum(n);
