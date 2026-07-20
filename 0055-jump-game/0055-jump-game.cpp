5547375d3a3737cd2c9f8844e1019a201f930e50
Ayush Kumar Jha <ayushkumarj932@gmail.com>
2026-07-20 21:10:13 +0530

    Time: 0 ms (100%), Space: 52.3 MB (83.75%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -6,7 +6,7 @@ public:
         for(int i=0;i<n;i++){
             if(i > maxi)
                 return false;
-            maxi = max(maxi,i + nums[i]);
+            maxi =max(maxi,i+nums[i]);
         }
         return true;
     }
