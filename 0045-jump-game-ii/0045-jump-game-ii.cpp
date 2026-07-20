5b1ceee28f996370eae232dd00a6985266bd5a15
Ayush Kumar Jha <ayushkumarj932@gmail.com>
2026-07-20 21:12:35 +0530

    Time: 0 ms (100%), Space: 20.7 MB (29.01%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -5,7 +5,7 @@ public:
         int l=0;
         int r=0;
         int jumps=0;
-        while(r<n-1){
+        while(r < n-1 ){
             int far=0;
             for(int i=l;i<=r;i++){
                 far = max(far,i+nums[i]);
